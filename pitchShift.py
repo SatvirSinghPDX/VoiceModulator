413937231f3276ade5d6372e95218397f9dcdf31
davhogan <52215314+davhogan@users.noreply.github.com>
2020-05-30 21:07:35 -0700

    Update pitchShift.py
    Changed pitch shift to work more like how it was described in class.

--- pitchShift.py
+++ pitchShift.py
@@ -1,56 +1,38 @@
-from scipy.io import wavfile as wv
 import numpy as np
 import playsound as ps
+import wave as wv
 
-#http://zulko.github.io/blog/2014/03/29/soundstretching-and-pitch-shifting-in-python/
-#This is where I found the code. It is a good starting place to work and modify.
+wr = wv.open('you-are-acting-so-weird.wav', 'r')
+ww = wv.open('new_test.wav', 'w')
 
-def speedx(snd_array, factor):
-    """ Speeds up / slows down a sound, by some factor. """
-    indices = np.round(np.arange(0, len(snd_array), factor))
-    indices = indices[indices < len(snd_array)].astype(int)
-    return snd_array[indices]
+par = list(wr.getparams())
+ww.setparams(par)
 
+frame_per_sec = wr.getframerate()//20
+file_sz = int(wr.getnframes()/frame_per_sec)
+shift = 100//20
 
-def stretch(snd_array, factor, window_size, h):
-    """ Stretches/shortens a sound, by some factor. """
-    phase = np.zeros(window_size)
-    hanning_window = np.hanning(window_size)
-    result = np.zeros(int(len(snd_array) / factor + window_size))
+for num in range(file_sz):
+    data = np.fromstring(wr.readframes(frame_per_sec), dtype=np.int16)
+    left = data[0::2]
+    right = data[1::2]
+    #Take DFT
+    left_freq = np.fft.rfft(left)
+    right_freq = np.fft.rfft(right)
+    #Scale It Up or Down
+    left_freq = np.roll(left_freq, shift)
+    right_freq = np.roll(right_freq, shift)
+    left_freq[0:shift] = 0
+    right_freq[0:shift] = 0
+    #Take inverse DFT
+    left = np.fft.irfft(left_freq)
+    right = np.fft.irfft(right_freq)
+    #Put it altogether
+    sig = np.column_stack((left, right)).ravel().astype(np.int16)
+    ww.writeframes(sig.tostring())
 
-    for i in np.arange(0, len(snd_array) - (window_size + h), h*factor):
-        i = int(i)
-        # Two potentially overlapping subarrays
-        a1 = snd_array[i: i + window_size]
-        a2 = snd_array[i + h: i + window_size + h]
-
-        # The spectra of these arrays
-        s1 = np.fft.fft(hanning_window * a1)
-        s2 = np.fft.fft(hanning_window * a2)
-
-        # Rephase all frequencies
-        phase = (phase + np.angle(s2/s1)) % 2*np.pi
-
-        a2_rephased = np.fft.ifft(np.abs(s2)*np.exp(1j*phase))
-        i2 = int(i/factor)
-        result[i2: i2 + window_size] += hanning_window*a2_rephased.real
-
-    # normalize (16bit)
-    result = ((2**(16-4)) * result/result.max())
-
-    return result.astype('int16')
-
-
-def pitchshift(snd_array, n, window_size=2**13, h=2**11):
-    """ Changes the pitch of a sound by ``n`` semitones. """
-    factor = 2**(1.0 * n / 12.0)
-    stretched = stretch(snd_array, 1.0/factor, window_size, h)
-    return speedx(stretched[window_size:], factor)
-
-test_len, test_sig = wv.read('you-are-acting-so-weird.wav')
-test_sig1 = test_sig[:,0] #Get only 1 side. Will need to figure out how to get it to work with both
-test = pitchshift(test_sig1,10)
-wv.write('test.wav', test_len, test)
+wr.close()
+ww.close()
 
 ps.playsound('you-are-acting-so-weird.wav')
-ps.playsound('test.wav')
+ps.playsound('new_test.wav')
