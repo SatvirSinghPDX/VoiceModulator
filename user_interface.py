21485b82b1a5e44c92ab9f3919d99ffb30783bbe
David Hogan <davhogan@pdx.edu>
2020-06-06 23:09:38 -0700

    Updated code to be more readable

--- user_interface.py
+++ user_interface.py
@@ -11,7 +11,6 @@ import numpy as np
 import playsound as ps
 import pyaudio
 import soundfile as sf
-from scipy import signal as sg
 from randomstr import randomstr
 from pydub import AudioSegment
 
@@ -38,56 +37,57 @@ myFont = font.Font(family='Helvetica', size=11, weight='bold')
 
 # creation and placement of effect buttons
 
-effect1 = tk.Button(app, text="Public Place", width=9, command=lambda: [ps.playsound(effect1_file)])
-effect1.place(relx=0.4, rely=0.5, anchor=tk.CENTER)
-effect1['font'] = myFont
-effect2 = tk.Button(app, text="Robot Voice", width=9, command=lambda: [ps.playsound(effect2_file)])
-effect2.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
-effect2['font'] = myFont
-effect3 = tk.Button(app, text="Helium", width=9, command=lambda: [ps.playsound(effect3_file)])
-effect3.place(relx=0.6, rely=0.5, anchor=tk.CENTER)
-effect3['font'] = myFont
-effect4 = tk.Button(app, text="Echo", width=9, command=lambda: [ps.playsound(effect4_file)])
-effect4.place(relx=0.4, rely=0.6, anchor=tk.CENTER)
-effect4['font'] = myFont
-effect5 = tk.Button(app, text="Chipmunk", width=9, command=lambda: [ps.playsound(effect5_file)])
-effect5.place(relx=0.5, rely=0.6, anchor=tk.CENTER)
-effect5['font'] = myFont
-effect6 = tk.Button(app, text="Slow-Mo", width=9, command=lambda: [ps.playsound(effect6_file)])
-effect6.place(relx=0.6, rely=0.6, anchor=tk.CENTER)
-effect6['font'] = myFont
-effect7 = tk.Button(app, text="Reverse", width=9, command=lambda: [ps.playsound(effect7_file)])
-effect7.place(relx=0.4, rely=0.7, anchor=tk.CENTER)
-effect7['font'] = myFont
-effect8 = tk.Button(app, text="Rain",  width=9, command=lambda: [ps.playsound(effect8_file)])
-effect8.place(relx=0.5, rely=0.7, anchor=tk.CENTER)
-effect8['font'] = myFont
-effect9 = tk.Button(app, text="Effect 9", width=9)
-effect9.place(relx=0.6, rely=0.7, anchor=tk.CENTER)
-effect9['font'] = myFont
+public_place_button = tk.Button(app, text="Public Place", width=9, command=lambda: [ps.playsound(public_place_effect_file)])
+public_place_button.place(relx=0.4, rely=0.5, anchor=tk.CENTER)
+public_place_button['font'] = myFont
+robot_button = tk.Button(app, text="Robot Voice", width=9, command=lambda: [ps.playsound(robot_effect_file)])
+robot_button.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+robot_button['font'] = myFont
+helium_button = tk.Button(app, text="Helium", width=9, command=lambda: [ps.playsound(helium_effect_file)])
+helium_button.place(relx=0.6, rely=0.5, anchor=tk.CENTER)
+helium_button['font'] = myFont
+echo_button = tk.Button(app, text="Echo", width=9, command=lambda: [ps.playsound(echo_effect_file)])
+echo_button.place(relx=0.4, rely=0.6, anchor=tk.CENTER)
+echo_button['font'] = myFont
+chipmunk_button = tk.Button(app, text="Chipmunk", width=9, command=lambda: [ps.playsound(chipmunk_effect_file)])
+chipmunk_button.place(relx=0.5, rely=0.6, anchor=tk.CENTER)
+chipmunk_button['font'] = myFont
+slowmo_button = tk.Button(app, text="Slow-Mo", width=9, command=lambda: [ps.playsound(slowmo_effect_file)])
+slowmo_button.place(relx=0.6, rely=0.6, anchor=tk.CENTER)
+slowmo_button['font'] = myFont
+reverse_button = tk.Button(app, text="Reverse", width=9, command=lambda: [ps.playsound(reverse_effect_file)])
+reverse_button.place(relx=0.4, rely=0.7, anchor=tk.CENTER)
+reverse_button['font'] = myFont
+rain_button = tk.Button(app, text="Rain",  width=9, command=lambda: [ps.playsound(rain_effect_file)])
+rain_button.place(relx=0.5, rely=0.7, anchor=tk.CENTER)
+rain_button['font'] = myFont
+regular_button = tk.Button(app, text="Regular", width=9, command=lambda: [ps.playsound(regular_effect_file)])
+regular_button.place(relx=0.6, rely=0.7, anchor=tk.CENTER)
+regular_button['font'] = myFont
 
 # disable the effect buttons when initially running the app
 
-effect1['state'] = 'disabled'
-effect2['state'] = 'disabled'
-effect3['state'] = 'disabled'
-effect4['state'] = 'disabled'
-effect5['state'] = 'disabled'
-effect6['state'] = 'disabled'
-effect7['state'] = 'disabled'
-effect8['state'] = 'disabled'
-effect9['state'] = 'disabled'
+public_place_button['state'] = 'disabled'
+robot_button['state'] = 'disabled'
+helium_button['state'] = 'disabled'
+echo_button['state'] = 'disabled'
+chipmunk_button['state'] = 'disabled'
+slowmo_button['state'] = 'disabled'
+reverse_button['state'] = 'disabled'
+rain_button['state'] = 'disabled'
+regular_button['state'] = 'disabled'
 
 deleteWavFiles['state'] = 'normal'
 
-effect1_file = ''
-effect2_file = ''
-effect3_file = ''
-effect4_file = ''
-effect5_file = ''
-effect6_file = ''
-effect7_file = ''
-effect8_file = ''
+public_place_effect_file = ''
+robot_effect_file = ''
+helium_effect_file = ''
+echo_effect_file = ''
+chipmunk_effect_file = ''
+slowmo_effect_file = ''
+reverse_effect_file = ''
+rain_effect_file = ''
+regular_effect_file = ''
 
 def delete_file(filename):
     if os.path.exists(filename):
@@ -96,7 +96,7 @@ def delete_file(filename):
         print(filename + ' DNE')
 
 
-def effect_1(filename):
+def public_effect(filename):
     data, fs = sf.read(filename)
     public, rfs = sf.read('public.wav')
     output = [0] * len(data)
@@ -105,30 +105,30 @@ def effect_1(filename):
     for i in range(0, len(data)):
         output[i] = data[i][0] + .25 * public[i % public_len][0]
 
-    global effect1_file
-    effect1_file = randomstr(length=10) + '.wav'
-    sf.write(effect1_file, output, fs)
+    global public_place_effect_file
+    public_place_effect_file = randomstr(length=10) + '.wav'
+    sf.write(public_place_effect_file, output, fs)
 
 
-def effect_2(filename):
-    global effect2_file
-    effect2_file = randomstr(length=10) + '.wav'
+def robot_effect(filename):
+    global robot_effect_file
+    robot_effect_file = randomstr(length=10) + '.wav'
     shift = 2500 // 100
-    pitch_shift(filename, effect2_file, shift)
+    pitch_shift(filename, robot_effect_file, shift)
 
 
-def effect_3(filename):
-    global effect3_file
-    effect3_file = randomstr(length=10) + '.wav'
+def helium_effect(filename):
+    global helium_effect_file
+    helium_effect_file = randomstr(length=10) + '.wav'
     shift = 6000 // 150
-    pitch_shift(filename, effect3_file, shift)
+    pitch_shift(filename, helium_effect_file, shift)
 
-def effect_4(filename):
+def echo_effect(filename):
     data, fs = sf.read(filename)
     delay = .25
     gain = .75
-    global effect4_file
-    effect4_file = randomstr(length=10) + '.wav'
+    global echo_effect_file
+    echo_effect_file = randomstr(length=10) + '.wav'
 
     delay_index = int(delay * fs)
     output = [0] * len(data)
@@ -137,10 +137,10 @@ def effect_4(filename):
         output[i] = data[i][0] + gain * data[i - delay_index][0]
 
     # print(output)
-    sf.write(effect4_file, output, fs)
+    sf.write(echo_effect_file, output, fs)
 
 
-def effect_5(filename):
+def chipmunk_effect(filename):
     CHANNELS = 1
     SWIDTH = 2
     Change_RATE = 3.2
@@ -149,10 +149,10 @@ def effect_5(filename):
     RATE = spf.getframerate()
     signal = spf.readframes(-1)
 
-    global effect5_file
-    effect5_file = randomstr(length=10) + '.wav'
+    global chipmunk_effect_file
+    chipmunk_effect_file = randomstr(length=10) + '.wav'
 
-    wf = wv.open(effect5_file, 'wb')
+    wf = wv.open(chipmunk_effect_file, 'wb')
     wf.setnchannels(CHANNELS)
     wf.setsampwidth(SWIDTH)
     wf.setframerate(RATE * Change_RATE)
@@ -160,7 +160,7 @@ def effect_5(filename):
     wf.close()
 
 
-def effect_6(filename):
+def slowmo_effect(filename):
     CHANNELS = 1
     SWIDTH = 2
     Change_RATE = 1.5
@@ -168,10 +168,10 @@ def effect_6(filename):
     spf = wv.open(filename, 'rb')
     RATE = spf.getframerate()
     signal = spf.readframes(-1)
-    global effect6_file
-    effect6_file = randomstr(length=10) + '.wav'
+    global slowmo_effect_file
+    slowmo_effect_file = randomstr(length=10) + '.wav'
 
-    wf = wv.open(effect6_file, 'wb')
+    wf = wv.open(slowmo_effect_file, 'wb')
     wf.setnchannels(CHANNELS)
     wf.setsampwidth(SWIDTH)
     wf.setframerate(RATE * Change_RATE)
@@ -179,18 +179,18 @@ def effect_6(filename):
     wf.close()
 
 
-def effect_7(filename):
+def reverse_effect(filename):
     loop = AudioSegment.from_wav(filename)
     reversed = loop.reverse()
-    global effect7_file
-    effect7_file = randomstr(length=10) + '.wav'
-    reversed.export(effect7_file, format="wav")
+    global reverse_effect_file
+    reverse_effect_file = randomstr(length=10) + '.wav'
+    reversed.export(reverse_effect_file, format="wav")
 
 
 counter = 28800
 running = False
 
-def effect_8(filename):
+def rain_effect(filename):
     data, fs = sf.read(filename)
     rain, rfs = sf.read('rain.wav')
     output = [0] * len(data)
@@ -199,9 +199,9 @@ def effect_8(filename):
     for i in range(0, len(data)):
         output[i] = data[i][0] + .1 * rain[i % rain_len][0]
 
-    global effect8_file
-    effect8_file = randomstr(length=10) + '.wav'
-    sf.write(effect8_file, output, fs)
+    global rain_effect_file
+    rain_effect_file = randomstr(length=10) + '.wav'
+    sf.write(rain_effect_file, output, fs)
 
 
 def counter_label(label):
@@ -279,6 +279,7 @@ def start_record():
     deleteWavFiles['state'] = 'disabled'
     global st
     st = 1
+    global regular_effect_file
     frames = []
     FORMAT = pyaudio.paInt16  # 16 bits per sample
     CHANNELS = 2
@@ -310,26 +311,27 @@ def start_record():
     wf.writeframes(b''.join(frames))
     wf.close()
     # enable the effect buttons when recording is finished
-    effect1['state'] = 'normal'
-    effect2['state'] = 'normal'
-    effect3['state'] = 'normal'
-    effect4['state'] = 'normal'
-    effect5['state'] = 'normal'
-    effect6['state'] = 'normal'
-    effect7['state'] = 'normal'
-    effect8['state'] = 'normal'
-    effect9['state'] = 'normal'
+    public_place_button['state'] = 'normal'
+    robot_button['state'] = 'normal'
+    helium_button['state'] = 'normal'
+    echo_button['state'] = 'normal'
+    chipmunk_button['state'] = 'normal'
+    slowmo_button['state'] = 'normal'
+    reverse_button['state'] = 'normal'
+    rain_button['state'] = 'normal'
+    regular_button['state'] = 'normal'
     # play the recorded audio
     ps.playsound(filename)
-
-    effect_1(filename)
-    effect_2(filename)
-    effect_3(filename)
-    effect_4(filename)
-    effect_5(filename)
-    effect_6(filename)
-    effect_7(filename)
-    effect_8(filename)
+    
+    regular_effect_file = filename
+    public_effect(filename)
+    robot_effect(filename)
+    helium_effect(filename)
+    echo_effect(filename)
+    chipmunk_effect(filename)
+    slowmo_effect(filename)
+    reverse_effect(filename)
+    rain_effect(filename)
 
 
 def stop_record():
